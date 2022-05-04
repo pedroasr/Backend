e9d1afcf6d1fdf16ca324a81a706ffd017cff31e
Pedro Antonio <pedroantoniosr96@gmail.com>
2022-05-05 01:35:52 +0200

    admin-service
    Añadidos servicios basicos del admin: añadir y eliminar peliculas.

--- src/database/admin-services.ts
+++ src/database/admin-services.ts
@@ -1,4 +1,4 @@
-//import { SQL_DB } from './maria-db';
+import { SQL_DB } from './maria-db';
 
 export type PostMovie = {
     name: string;
@@ -14,3 +14,20 @@ export type AdminServices = {
  //   updateMovie(user: User): Promise<[string, boolean]>;
     deleteMovie(movieId: number): Promise<boolean>;
 };
+
+export function buildMovieServices(db: SQL_DB): AdminServices {
+    return {
+        async postMovie(movie: PostMovie): Promise<boolean>{
+            const queryPost = await db.runQuery(
+                `INSERT INTO movies (name, image, description, gender, release_year, rate) VALUES (${movie.name}, ${movie.image}, ${movie.description}, ${movie.gender}, ${movie.release_year}, ${movie.rate});`
+            );
+            return queryPost ? true : false;
+        },
+        async deleteMovie(movieId: number): Promise<boolean>{
+            const queryDelete = await db.runQuery(
+                `DELETE * FROM movies WHERE id=${movieId};`
+            );
+            return queryDelete ? true : false;
+        }
+    }
+}
